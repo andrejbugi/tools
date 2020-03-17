85a28eb9badcdc8634a2da7ad7502bb852c84cad
andrejbugi <andrejbugi96@gmail.com>
2020-03-17 22:22:14 +0100

    new method, all test pass

--- lib/tools.rb
+++ lib/tools.rb
@@ -7,22 +7,88 @@ module Tools
     end
 
     def valid?
-      return false if @str.length <= 1
+      return false if @str.length < 2
+      return false if divide_by_10? % 10 != 0
+      return false if @stripped.scan(/\D/).any? || @reversed_array.length < 2
 
+      true
+
+    end
+
+    private
+
+    # def strip_reverse
+    #   stripped = @str.delete(' ')
+    #   @stripped = stripped
+    #   reversed_strip = stripped.reverse
+    #   reversed_array = reversed_strip.chars.map(&:to_i)
+    #   @reversed_array = reversed_array
+    # end
+
+    # def every_first
+    #   reversed_array = @reversed_array
+    #   every_first_range = (0..reversed_array.length - 1).step(2)
+    #   every_first = every_first_range.map { |i| reversed_array[i] }
+    #   @every_first = every_first.sum
+    # end
+
+    # def every_second
+    #   reversed_array = @reversed_array
+    #   every_second_range = (1..reversed_array.length - 1).step(2)
+    #   every_second_array = every_second_range.map { |i| reversed_array[i] }
+    #   @every_second_array = every_second_array
+    # end
+
+    # def second_doubled
+    #   every_second_array = @every_second_array
+    #   every_second = []
+    #   every_second_array.each do |num|
+    #     num += num
+    #     if num >= 10
+    #       num = num - 9
+    #     else
+    #       num
+    #     end
+    #     every_second << num
+    #   end
+    #   sum_index = 0
+    #   every_second.each do |index|
+    #     sum_index += index
+    #   end
+    #   @sum_index = sum_index
+    # end
+
+    def divide_by_10?
       stripped = @str.delete(' ')
+      @stripped = stripped
       puts "strip_str: #{stripped}"
       reversed_strip = stripped.reverse
       puts "reversed_strip: #{reversed_strip}"
-      reversed_array = reversed_strip.chars
+      reversed_array = reversed_strip.chars.map(&:to_i)
+      @reversed_array = reversed_array
       puts "reversed_array: #{reversed_array}"
-      every_second_range = (1..reversed_array.length-1).step(2)
+      every_second_range = (1..reversed_array.length - 1).step(2)
       every_second_array = every_second_range.map { |i| reversed_array[i] }
       puts "every_second_array: #{every_second_array}"
-
-
-      true
+      every_first_range = (0..reversed_array.length - 1).step(2)
+      every_first = every_first_range.map { |i| reversed_array[i] }
+      puts every_first.sum
+      every_second = []
+      every_second_array.each do |num|
+        num += num
+        if num >= 10
+          num = num - 9
+        else
+          num
+        end
+        every_second << num
+      end
+      sum_index = 0
+      every_second.each do |index|
+        sum_index += index
+      end
+      sum_index + every_first.sum
     end
-
 
   end
 end
--- spec/luhn_spec.rb
+++ spec/luhn_spec.rb
@@ -33,4 +33,49 @@ RSpec.describe Tools::Luhn do
     luhn_example = Tools::Luhn.new('055 444 286')
     expect(luhn_example.valid?).to be false
   end
+
+  it 'is invalid credit card' do
+    luhn_example = Tools::Luhn.new('8273 1232 7352 0569')
+    expect(luhn_example.valid?).to be false
+  end
+
+  it 'becomes invalid with non digit' do
+    luhn_example = Tools::Luhn.new('055a 444 285')
+    expect(luhn_example.valid?).to be false
+  end
+
+  it 'becomes invalid with non digit at the end' do
+    luhn_example = Tools::Luhn.new('059a')
+    expect(luhn_example.valid?).to be false
+  end
+
+  it 'becomes invalid with punctuations included' do
+    luhn_example = Tools::Luhn.new('055-444-285')
+    expect(luhn_example.valid?).to be false
+  end
+
+  it 'becomes invalid with symbols included' do
+    luhn_example = Tools::Luhn.new('055$ 444$ 285')
+    expect(luhn_example.valid?).to be false
+  end
+
+  it 'is invalid with space and single zero' do
+    luhn_example = Tools::Luhn.new(' 0')
+    expect(luhn_example.valid?).to be false
+  end
+
+  it 'is valid with more than one zero' do
+    luhn_example = Tools::Luhn.new('0000 0')
+    expect(luhn_example.valid?).to be true
+  end
+
+  it 'is invalid with non digits' do
+    luhn_example = Tools::Luhn.new(':9')
+    expect(luhn_example.valid?).to be false
+  end
+
+  it 'returns input digit 9 as digit 9 in output' do
+    luhn_example = Tools::Luhn.new('091')
+    expect(luhn_example.valid?).to be true
+  end
 end
