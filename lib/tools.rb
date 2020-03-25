05ab10ac09cd5c93434d5e33707e780a2db4a5f2
andrejbugi <andrejbugi96@gmail.com>
2020-03-25 14:30:52 +0100

    simplified methods

--- lib/tools.rb
+++ lib/tools.rb
@@ -9,7 +9,7 @@ module Tools
     def valid?
       return false if @stripped.length < 2
       return false if @stripped.scan(/\D/).any?
-      return false if checksum % 10 != 0
+      return false if (rest_of_digits + checksum).sum % 10 != 0
 
       true
 
@@ -22,31 +22,26 @@ module Tools
     end
 
     def every_sec_digit
-      every_second_range = (1..@stripped.length - 1).step(2)
-      every_second_range.map { |i| reverse[i].to_i }
+      indexes(1).map { |i| reverse[i].to_i }
     end
 
     def rest_of_digits
-      every_first_range = (0..@stripped.length - 1).step(2)
-      every_first_range.map { |i| reverse[i].to_i }
+      indexes.map { |i| reverse[i].to_i }
+    end
+
+    def indexes(ind = 0)
+      (ind..@stripped.length - 1).step(2)
     end
 
     def checksum
-      every_second = []
-      every_sec_digit.each do |num|
+      every_sec_digit.map do |num|
         num += num
         if num >= 10
           num -= 9
         else
           num
         end
-        every_second << num
       end
-      sum_index = 0
-      every_second.each do |index|
-        sum_index += index
-      end
-      sum_index + rest_of_digits.sum
     end
   end
 
